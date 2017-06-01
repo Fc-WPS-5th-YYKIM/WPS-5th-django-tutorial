1fb597f38d7e6b9d24edc9153c82fef06cbc6fb3
yykdev <assavictory@gmail.com>
2017-06-01 12:58:51 +0900

    add polls application to INSTALLED_APPS, create Question, Choice models, make DB migrations

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField('질문내용', max_length=200)
+    pub_date = models.DateTimeField('발행일자')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        verbose_name='해당 질문',
+        on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # Model : DB와 데이터 송수신을 위한 모듈
+    # Model에 생성한 클래스에서 사용하는 변수는 DB 테이블의 컬럼과 일치해야 함
+    # ForeignKey dml 필드명은 verbose_name 옵션에 넣어줘야 함.
